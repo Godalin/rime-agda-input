a02be0deda52e3b78bec09861cc5586b7d9914b3
Linyu Yang <yly1228@foxmail.com>
2024-09-17 16:53:36 +0800

    updated: dict, recover \\

--- make_dict.py
+++ make_dict.py
@@ -18,10 +18,7 @@ matches = pattern.finditer(content)
 
 
 def add_single_symbol(key: str, symbol, file):
-    if key.startswith(("^", "_")):
-        pass
-    else:
-        key = "\\" + key
+    key = "\\" + key
     print(symbol, key, sep="\t", file=file)
 
 def add_symbol_list(key, symbols, file):
